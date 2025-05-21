c68a218a2d8e6f7081fd2471b5fd272180c63983
wilson1202 <ibox_wilson@outlook.com>
2025-05-21 19:58:51 +0800

    Update v1.0.0-rc.151

---
+++ docs/.vuepress/dist/assets/classDiagram-v2-PRA2ZCF7-RfAbquyd.js
@@ -0,0 +1,1 @@
+import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-BmMKXIXB.js";import"./chunk-IO347I67-DvnkeIbJ.js";import{m as o}from"./mermaid.esm.min-DhcxcZa4.js";import"./app-D0oWhVB0.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
