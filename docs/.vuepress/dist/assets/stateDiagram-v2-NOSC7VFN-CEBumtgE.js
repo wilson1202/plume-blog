4f099891ae5868182443bdce01a7963640a5e074
wilson1202 <ibox_wilson@outlook.com>
2025-06-17 18:27:50 +0800

    1

---
+++ docs/.vuepress/dist/assets/stateDiagram-v2-NOSC7VFN-CEBumtgE.js
@@ -0,0 +1,1 @@
+import{m as e,s,j as t,x as a}from"./chunk-D7JRX7UM-CbmTquiA.js";import"./chunk-IO347I67-vL2riXDF.js";import{m}from"./mermaid.esm.min-vtafHnmq.js";import"./app-B6Zsp4mc.js";var b={parser:t,get db(){return new a(2)},renderer:s,styles:e,init:m(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{b as diagram};
--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -25,8 +25,8 @@ export const navbar = defineNavbarConfig([
   // },
 
   {
-    text: '豆瓣',
-    icon: 'mingcute:building-4-fill',
+    text: '时光',
+    icon: 'streamline-logos:douban-logo-solid',
     items: [
       { text: '读书', link: '/notes/豆瓣/1.book.md' },
       { text: '电影', link: '/notes/豆瓣/2.movie.md' },
