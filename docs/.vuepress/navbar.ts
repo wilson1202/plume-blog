3223517c710adb8d38208507266b591b47232f82
wilson1202 <ibox_wilson@outlook.com>
2025-06-20 21:56:22 +0800

    1

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -28,10 +28,10 @@ export const navbar = defineNavbarConfig([
     text: '时光',
     icon: 'streamline-logos:douban-logo-solid',  
     items: [
-      { text: '读书', link: '/notes/豆瓣/1.book.md' },
-      { text: '电影', link: '/notes/豆瓣/2.movie.md' },
-      { text: '音乐', link: '/notes/豆瓣/3.song.md' },
-      { text: '游戏', link: '/notes/豆瓣/4.game.md' }
+      { text: '读书', link: '/notes/豆瓣/1.book.md', icon: 'ic:twotone-menu-book' },
+      { text: '电影', link: '/notes/豆瓣/2.movie.md', icon: 'si:movie-duotone' },
+      { text: '音乐', link: '/notes/豆瓣/3.song.md', icon: 'bi:music-player-fill' },
+      { text: '游戏', link: '/notes/豆瓣/4.game.md', icon: 'icon-park-twotone:gamepad' }
     ]
   }
 ])
