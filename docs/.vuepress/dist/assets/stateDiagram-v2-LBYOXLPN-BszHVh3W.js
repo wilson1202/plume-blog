bba60588a1feb3e847ffd7635fe423e9e13dbed0
wilson1202 <ibox_wilson@outlook.com>
2025-06-21 11:18:28 +0800

    调整 twikoo 默认样式

---
+++ docs/.vuepress/dist/assets/stateDiagram-v2-LBYOXLPN-BszHVh3W.js
@@ -0,0 +1,1 @@
+import{g as e,q as t,B as a,b as s}from"./chunk-Q7NYVSYU-DwGS4lJe.js";import"./chunk-NXEA5HOC-B49adH__.js";import{m as o}from"./mermaid.esm.min-B2S4Z1h7.js";import"./app-C3SXNoKB.js";var n={parser:a,get db(){return new s(2)},renderer:t,styles:e,init:o(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{n as diagram};
